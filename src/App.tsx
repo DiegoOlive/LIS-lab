640e2546a6b615df13f326765cb1b45310eb57a8
Your Name <you@example.com>
2020-10-11 16:08:04 -0300

    linha em branco

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
+
 
   return (
-    <div className="App">     
+    <div className="App">   
+      
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
