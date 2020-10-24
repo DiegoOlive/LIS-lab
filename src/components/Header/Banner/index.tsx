1a7dd11006450a319343ddc93c17ebe1d7d62022
Your Name <you@example.com>
2020-10-24 00:25:43 -0300

    GRID aula 16

--- src/components/Header/Banner/index.tsx
+++ src/components/Header/Banner/index.tsx
@@ -2,15 +2,37 @@ import React from 'react';
 import styled from 'styled-components';
 import Img from '../../../assets/images/home/inicio.svg';
 import BannerImg from '../BannerImg/';
-import BannerMessager from '../BannerMessager';
+import BannerMessager, { MessageContainer } from '../BannerMessager';
+
 
 //flex-direction: row;
 //justify-content:center;
 const BannerContainer = styled.div`
     display: flex;
     flex-direction: column;
-    align-items:center;    
+    align-items:center;       
+
+    @media (min-width: 768px){
+        display: grid;
+        grid-template-columns: 3fr 2fr;
+        grid-template-areas:
+            "message figure";
+
+        & ${BannerImg}{
+            grid-area: figure;
+            justify-self: end;           
+        } 
+        
+        & ${MessageContainer}{
+            grid-area: message;
+        }
+
+             
+
+    }
+
 `
+//grid para separar as coisas
 
 export default function Banner(){
     return(
@@ -21,5 +43,5 @@ export default function Banner(){
             />
             <BannerMessager />
         </BannerContainer>
-    )    
+    );    
 }
